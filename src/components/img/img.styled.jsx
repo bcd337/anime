53111d4f3cf2e086d72c2089d6c6b667fa8da812
iqbal <b.bcd337@gmail.com>
2022-06-26 16:37:07 +0700

    set fetchpriority=low for img

--- src/components/img/img.styled.jsx
+++ src/components/img/img.styled.jsx
@@ -1,10 +1,12 @@
 import styled from '@emotion/styled'
 
+const Img = ({ onLoad, alt, load, ...props }) => <img onLoad={onLoad} alt={alt} {...props} fetchpriority="low" loading="lazy" crossOrigin="anonymous" />
+
 export const ContainerImage = styled.div`
   background: linear-gradient(180deg, var(--light) 40%, var(--shadow-dark));
 `
 
-export const Image = styled.img<{ load: boolean }>`
+export const Image = styled(Img)`
   object-fit: cover;
   background: linear-gradient(180deg, var(--light) 40%, var(--shadow-dark));
   opacity: ${(props) => props.load ? 1 : 0};
